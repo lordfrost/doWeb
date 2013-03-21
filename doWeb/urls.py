591809e16b2b4797b6305f6eaa1098188fbf68f0
lordfrost <xb1jke@gmail.com>
2013-03-21 16:36:06 +0600

    i don't know what is that ))

--- doWeb/urls.py
+++ doWeb/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from doit.views import hello
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'doWeb.views.home', name='home'),
     # url(r'^doWeb/', include('doWeb.foo.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
 )
