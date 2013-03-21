470f9b7968e33da8b18e53dec6b52f539a7969cc
lordfrost <xb1jke@gmail.com>
2013-03-21 18:38:22 +0600

    it's all right

--- doit/views.py
+++ doit/views.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse("Здравствуй, мир")
+
+def datetimenow(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" %now
+    return HttpResponse(html)
